417d6633ee31a3bbeebb835bc3fa233a39757e22
man in the crowd <34878107+TunirWilliams@users.noreply.github.com>
2022-03-15 04:09:59 +0530

    Added nqueens.cpp

---
+++ nqueen.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <utility>
+#include <vector>
+#define N 4
+using namespace std;
+
+typedef pair<int,int> position;
+position queens[N];
+vector<position> postack;
+int board[N];
+int main()
+{
+    int i,j,k;
+    board[0]=0;
+    while(board[0]<N)
+    {
+        i++;
+    }
+    return 0;
+}
