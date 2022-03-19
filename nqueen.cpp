86abad42e8488804f2fa1857fb63ea7c672ec3f5
man in the crowd <34878107+TunirWilliams@users.noreply.github.com>
2022-03-20 03:39:39 +0530

    basics

--- nqueen.cpp
+++ nqueen.cpp
@@ -6,15 +6,40 @@ using namespace std;
 
 typedef pair<int,int> position;
 //position queens[N];
+int tos;
+bool emp=false;
 vector<position> postack;
+
+position queen;
 int board[N];
 int main()
 {
-    int i,j,k;
+    int i=1,j=0,k;
     board[0]=0;
     while(board[0]<N)
     {
-        i++;
+        queen.first=i;
+        queen.second=j;
+        if(checkpos(queen))
+        {
+            postack.push_back(queen);
+            board[i]=j;
+            i++;
+            j=0;
+
+        }
+        else
+        {
+            j++;
+            if(j>=N)
+            {
+                queen=postack[tos];
+                postack.pop_back();
+                i--;
+                j=(queen.second)+1;
+                
+            }
+        }
     }
     return 0;
 }
