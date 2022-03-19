acc8b16efe8a7d73ff639daae9e3e7b5c101fb87
man in the crowd <34878107+TunirWilliams@users.noreply.github.com>
2022-03-20 04:04:22 +0530

    First draft

--- nqueen.cpp
+++ nqueen.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 typedef pair<int,int> position;
 //position queens[N];
-int tos;
+int tos=0;
 bool emp=false;
 vector<position> postack;
 
 position queen;
 int board[N];
+bool checkpos(position q);
 int main()
 {
-    int i=1,j=0,k;
+    int i=1,j=0,c=0;
     board[0]=0;
     while(board[0]<N)
     {
@@ -24,7 +25,9 @@ int main()
         {
             postack.push_back(queen);
             board[i]=j;
+            tos++;
             i++;
+            if(i==N) c++;
             j=0;
 
         }
@@ -35,11 +38,15 @@ int main()
             {
                 queen=postack[tos];
                 postack.pop_back();
+                tos--;
+                if(tos==0)
+                board[0]++;
                 i--;
                 j=(queen.second)+1;
                 
             }
         }
     }
+    cout<<"Total config: "<<c<<endl;
     return 0;
 }
